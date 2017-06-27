8b15e5cc78e5912f790a45363cb8d261624fb474
David Stonikas <dstonikas@gmail.com>
2017-06-27 14:09:11 -0400

    Adjusted routes to declutter app.js

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
+const flash = require('connect-flash');
+const session = require('express-session');
 
 mongoose.connect('mongodb://localhost/nodekb');
 let db = mongoose.connection;
@@ -20,6 +23,37 @@ db.on('error', function(err){
 // Init App
 const app = express();
 
+// Express Session Middleware
+app.use(session({
+  secret: 'keyboard cat',
+  resave: true,
+  saveUninitialized: true
+}))
+
+//Express Messages Middleware
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
+
+//Express Validator Middleware
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
 
 // Bring in Models
 let Article = require('./models/article');
@@ -52,78 +86,9 @@ app.get('/', function(req, res){
 
 });
 
-// Add Route
-app.get('/articles/add', function(req, res){
-  res.render('add_article',{
-    title: 'Add Article'
-  });
-});
-
-// Add Submit POST Route
-app.post('/articles/add', function(req, res){
-  let article = new Article();
-  article.title = req.body.title;
-  article.author = req.body.author;
-  article.body = req.body.body;
-
-  article.save(function(err){
-    if(err){
-      console.log(err);
-      return;
-    } else {
-      res.redirect('/');
-    }
-  });
-});
-
-// Load Edit Form
-app.get('/article/edit/:id', function(req, res){
-  Article.findById(req.params.id, function(err, article){
-    res.render('edit_article', {
-      article: article
-    });
-  });
-});
-
-// Get single Article
-app.get('/article/:id', function(req, res){
-  Article.findById(req.params.id, function(err, article){
-    res.render('article', {
-      article: article
-    });
-  });
-});
-
-// Update Submit POST Route
-app.post('/articles/edit/:id', function(req, res){
-  let article = {};
-  article.title = req.body.title;
-  article.author = req.body.author;
-  article.body = req.body.body;
-
-  let query ={_id:req.params.id}
-
-  Article.update(query, article, function(err){
-    if(err){
-      console.log(err);
-      return;
-    } else {
-      res.redirect('/');
-    }
-  });
-});
-
-// Delete article Route
-app.delete('/article/:id', function(req, res){
-  let query = {_id:req.params.id}
-
-  Article.remove(query, function(err){
-    if(err){
-      console.log(err);
-    }
-    res.send('Success');
-  });
-});
+// Route Files
+let articles = require('./routes/articles');
+app.use('/articles', articles);
 
 // Start Server
 app.listen(3000, function(){
